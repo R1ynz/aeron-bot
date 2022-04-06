068af608631fc90aed12f373c5b766178744e3e2
Riyan <100784430+R1ynz@users.noreply.github.com>
2022-04-06 13:29:25 +0700

    update aeron.js

--- aeron.js
+++ aeron.js
@@ -109,6 +109,8 @@ reply("error")
 .on('end', function () {
 console.log('Selesai convert')
 aeron.sendMessage(from, { sticker: {url: './image/sticker.webp'}, mimetype: 'image/webp' })
+fs.unlinkSync(media)
+fs.unlinkSync(ran)
 })
 .addOutputOptions([`-vcodec`, `libwebp`, `-vf`, `scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
 .toFormat('webp')
