2cff54dc9f11a6d9caad17720eb40b4cd7fba7b7
Riyan <100784430+R1ynz@users.noreply.github.com>
2022-04-07 09:17:01 +0700

    update aeron.js

--- aeron.js
+++ aeron.js
@@ -109,7 +109,6 @@ aeron.sendMessage(from, { sticker: {url: './rubbish/mysticker2.webp' }, mimetype
 .addOutputOptions(["-vcodec", "libwebp", "-vf", "scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse"])
 .toFormat('webp')
 .save('./rubbish/mysticker2.webp')
-
 } else {
 reply('error')
 }
